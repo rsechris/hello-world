1a82df5f6cea58ba5178a58987b7aeeb0376a2c6
rsechris <34580163+rsechris@users.noreply.github.com>
2018-01-26 14:20:21 -0500

    Update report_rates.m

--- report_rates.m
+++ report_rates.m
@@ -4,9 +4,9 @@
 shtepo = load('C:\2D\Andy\BRC_Kinetics\16-12-14\10state_P+B-_LLS_4\opt-04-18-01-25-final.mat');
 vqe = shtepo.model.params.taumat.val;
 
-% list = [4,17,35,49,93,123,153]; % original 8 state model
+list = [4,17,35,49,93,123,153]; % original 8 state model
 % listd = [4,19,39,55,105,143,235,173]; % 9 state model
-list = [4,23,47,67,129,171,213,135,297,339,253]; % 11 state model
+% list = [4,23,47,67,129,171,213,135,297,339,253]; % 11 state model
 % list = [4,21,43,61,117,155,193,229,159,269,305]; % 10 state model
 
 clear bumba; % original 8 state model
